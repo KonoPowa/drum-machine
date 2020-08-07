28ed637a1e3188ed4c4bd277b66cdfa2457cb148
Yacob Abusadah <yacob.abusadah@gmail.com>
2020-08-06 21:48:20 -0400

    Updates

---
+++ precache-manifest.9d54d19df1ca4c65dfd30fea1fe9d535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa8ce787576f2d0192b414729b3cd04",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "9496cbc477dbc5a9e7e1",
+    "url": "/drum-machine/static/css/main.103dd414.chunk.css"
+  },
+  {
+    "revision": "b9b610ce1800dd398488",
+    "url": "/drum-machine/static/js/2.293e08c0.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/drum-machine/static/js/2.293e08c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9496cbc477dbc5a9e7e1",
+    "url": "/drum-machine/static/js/main.50a0bf4e.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
